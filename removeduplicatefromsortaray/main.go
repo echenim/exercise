bf901456fe5c009f3bd5f20db435011085aad27a
echenim <myron.echenim@gmail.com>
2021-05-29 22:28:19 -0700

    Remove Duplicates from Sorted Array

---
+++ removeduplicatefromsortaray/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	sa := []int{1, 1, 2}
+
+	k := removeDuplicates(sa)
+
+	fmt.Println(k)
+}
+
+func removeDuplicates(nums []int) int {
+	l := 0
+	if len(nums) == 0 {
+		return l
+	}
+
+	for i := 1; i < len(nums); i++ {
+		if nums[l] != nums[i] {
+			l++
+			nums[l] = nums[i]
+		}
+	}
+
+	return l + 1
+}
